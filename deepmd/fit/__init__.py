351dc0c165b84b22808e9798ca2892c78f77c56b
mingzhong15 <46273005+mingzhong15@users.noreply.github.com>
2021-06-21 19:00:35 +0800

    Update __init__.py

--- deepmd/fit/__init__.py
+++ deepmd/fit/__init__.py
@@ -1,4 +1,5 @@
 from .ener      import EnerFitting
+from .dos      import DOSFitting
 from .wfc       import WFCFitting
 from .dipole    import DipoleFittingSeA
 from .polar     import PolarFittingSeA
