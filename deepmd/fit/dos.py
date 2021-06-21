8da81282dffdb269daf6e9e93bd26b1d6d12d4ae
mingzhong15 <46273005+mingzhong15@users.noreply.github.com>
2021-06-21 15:44:55 +0800

    DOS fitting
    1. delete 'self.atom_ener'
    2. delete 'self.tot_ener_zero'
    3. `compute_output_state` would not be performed in the model.py (although not deleted)
    4. add relevant argument for DOS fitting (numb_dos, ener_min, ener_max)

--- deepmd/fit/dos.py
+++ deepmd/fit/dos.py
@@ -12,7 +12,7 @@ from deepmd.descriptor import DescrptSeA
 from deepmd.env import global_cvt_2_tf_float
 from deepmd.env import GLOBAL_TF_FLOAT_PRECISION
 
-class EnerFitting ():
+class DOSFitting ():
     @docstring_parameter(list_to_doc(ACTIVATION_FN_DICT.keys()), list_to_doc(PRECISION_DICT.keys()))
     def __init__ (self, 
                   descrpt : tf.Tensor,
@@ -20,11 +20,12 @@ class EnerFitting ():
                   resnet_dt : bool = True,
                   numb_fparam : int = 0,
                   numb_aparam : int = 0,
+                  numb_dos: int = 300,
+                  ener_min: float = -10,
+                  ener_max: float = 10,
                   rcond : float = 1e-3,
-                  tot_ener_zero : bool = False,
                   trainable : List[bool] = None,
                   seed : int = 1,
-                  atom_ener : List[float] = [],
                   activation_function : str = 'tanh',
                   precision : str = 'default'
     ) -> None:
@@ -45,16 +46,18 @@ class EnerFitting ():
                 Number of atomic parameter
         rcond
                 The condition number for the regression of atomic energy.
-        tot_ener_zero
-                Force the total energy to zero. Useful for the charge fitting.
+        ! numb_dos (added)
+                Number of gridpoints on which the DOS is evaluated (NEDOS in VASP)
+        ! ener_min (added)
+                Lower boundary of the energy range for the evaluation of the DOS
+        ! ener_max (added)
+                Upper boundary of the energy range for the evaluation of the DOS
         trainable
                 If the weights of fitting net are trainable. 
                 Suppose that we have N_l hidden layers in the fitting net, 
                 this list is of length N_l + 1, specifying if the hidden layers and the output layer are trainable.
         seed
                 Random seed for initializing the network parameters.
-        atom_ener
-                Specifying atomic energy contribution in vacuum. The `set_davg_zero` key in the descrptor should be set.
         activation_function
                 The activation function in the embedding net. Supported options are {0}
         precision
@@ -63,25 +66,17 @@ class EnerFitting ():
         # model param
         self.ntypes = descrpt.get_ntypes()
         self.dim_descrpt = descrpt.get_dim_out()
-        # args = ()\
-        #        .add('numb_fparam',      int,    default = 0)\
-        #        .add('numb_aparam',      int,    default = 0)\
-        #        .add('neuron',           list,   default = [120,120,120], alias = 'n_neuron')\
-        #        .add('resnet_dt',        bool,   default = True)\
-        #        .add('rcond',            float,  default = 1e-3) \
-        #        .add('tot_ener_zero',    bool,   default = False) \
-        #        .add('seed',             int)               \
-        #        .add('atom_ener',        list,   default = [])\
-        #        .add("activation_function", str,    default = "tanh")\
-        #        .add("precision",           str, default = "default")\
-        #        .add("trainable",        [list, bool], default = True)
         self.numb_fparam = numb_fparam
         self.numb_aparam = numb_aparam
         self.n_neuron = neuron
         self.resnet_dt = resnet_dt
         self.rcond = rcond
+        
+        self.numb_dos = numb_dos
+        self.ener_min = ener_min
+        self.ener_max = ener_max
+        
         self.seed = seed
-        self.tot_ener_zero = tot_ener_zero
         self.fitting_activation_fn = get_activation_func(activation_function)
         self.fitting_precision = get_precision(precision)
         self.trainable = trainable
@@ -90,14 +85,9 @@ class EnerFitting ():
         if type(self.trainable) is bool:
             self.trainable = [self.trainable] * (len(self.n_neuron)+1)
         assert(len(self.trainable) == len(self.n_neuron) + 1), 'length of trainable should be that of n_neuron + 1'
-        self.atom_ener = []
-        for at, ae in enumerate(atom_ener):
-            if ae is not None:
-                self.atom_ener.append(tf.constant(ae, GLOBAL_TF_FLOAT_PRECISION, name = "atom_%d_ener" % at))
-            else:
-                self.atom_ener.append(None)
+
         self.useBN = False
-        self.bias_atom_e = None
+        self.bias_dos = None
         # data requirement
         if self.numb_fparam > 0 :
             add_data_requirement('fparam', self.numb_fparam, atomic=False, must=True, high_prec=False)
@@ -110,6 +100,15 @@ class EnerFitting ():
             self.aparam_std = None
             self.aparam_inv_std = None
 
+    def get_numb_dos(self) -> int:
+        return self.numb_dos
+
+    def get_ener_min(self) -> float:
+        return self.ener_min
+
+    def get_ener_max(self) -> float:
+        return self.ener_max            
+            
     def get_numb_fparam(self) -> int:
         """
         Get the number of frame parameters
@@ -131,14 +130,14 @@ class EnerFitting ():
         ----------
         all_stat
                 must have the following components:
-                all_stat['energy'] of shape n_sys x n_batch x n_frame
+                all_stat['dos'] of shape n_sys x n_batch x n_frame
                 can be prepared by model.make_stat_input
         """
-        self.bias_atom_e = self._compute_output_stats(all_stat, rcond = self.rcond)
+        self.bias_dos = self._compute_output_stats(all_stat, rcond = self.rcond)
 
     @classmethod
     def _compute_output_stats(self, all_stat, rcond = 1e-3):
-        data = all_stat['energy']
+        data = all_stat['dos']
         # data[sys_idx][batch_idx][frame_idx]
         sys_ener = np.array([])
         for ss in range(len(data)):
@@ -147,7 +146,7 @@ class EnerFitting ():
                 for jj in range(len(data[ss][ii])):
                     sys_data.append(data[ss][ii][jj])
             sys_data = np.concatenate(sys_data)
-            sys_ener = np.append(sys_ener, np.average(sys_data))
+            sys_dos = np.append(sys_dos, np.average(sys_data))
         data = all_stat['natoms_vec']
         sys_tynatom = np.array([])
         nsys = len(data)
@@ -155,9 +154,9 @@ class EnerFitting ():
             sys_tynatom = np.append(sys_tynatom, data[ss][0].astype(np.float64))
         sys_tynatom = np.reshape(sys_tynatom, [nsys,-1])
         sys_tynatom = sys_tynatom[:,2:]
-        energy_shift,resd,rank,s_value \
-            = np.linalg.lstsq(sys_tynatom, sys_ener, rcond = rcond)
-        return energy_shift    
+        dos_shift,resd,rank,s_value \
+            = np.linalg.lstsq(sys_tynatom, sys_dos, rcond = rcond)
+        return dos_shift    
 
     def compute_input_stats(self, 
                             all_stat : dict,
@@ -235,10 +234,10 @@ class EnerFitting ():
                 Name suffix to identify this descriptor
         Return
         ------
-        ener
-                The system energy
+        dos
+                The system density of state
         """
-        bias_atom_e = self.bias_atom_e
+        bias_dos = self.bias_dos
         if self.numb_fparam > 0 and ( self.fparam_avg is None or self.fparam_inv_std is None ):
             raise RuntimeError('No data stat result. one should do data statisitic, before build')
         if self.numb_aparam > 0 and ( self.aparam_avg is None or self.aparam_inv_std is None ):
@@ -251,6 +250,10 @@ class EnerFitting ():
             t_daparam = tf.constant(self.numb_aparam, 
                                     name = 'daparam', 
                                     dtype = tf.int32)
+            t_numb_dos = tf.constant(self.numb_dos,
+                                    name = 'numb_dos',
+                                    dtype = tf.int32)
+            
             if self.numb_fparam > 0: 
                 t_fparam_avg = tf.get_variable('t_fparam_avg', 
                                                self.numb_fparam,
@@ -277,8 +280,8 @@ class EnerFitting ():
         start_index = 0
         inputs = tf.cast(tf.reshape(inputs, [-1, self.dim_descrpt * natoms[0]]), self.fitting_precision)
 
-        if bias_atom_e is not None :
-            assert(len(bias_atom_e) == self.ntypes)
+        if bias_dos is not None :
+            assert(len(bias_dos) == self.ntypes)
 
         if self.numb_fparam > 0 :
             fparam = input_dict['fparam']
@@ -311,48 +314,25 @@ class EnerFitting ():
                 layer = tf.concat([layer, ext_aparam], axis = 1)
             start_index += natoms[2+type_i]
                 
-            if bias_atom_e is None :
-                type_bias_ae = 0.0
+            if bias_dos is None :
+                type_bias_ados = 0.0
             else :
-                type_bias_ae = bias_atom_e[type_i]
+                type_bias_ados = bias_dos[type_i]
 
             for ii in range(0,len(self.n_neuron)) :
                 if ii >= 1 and self.n_neuron[ii] == self.n_neuron[ii-1] :
                     layer+= one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, use_timestep = self.resnet_dt, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, trainable = self.trainable[ii])
                 else :
                     layer = one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, trainable = self.trainable[ii])
-            final_layer = one_layer(layer, 1, activation_fn = None, bavg = type_bias_ae, name='final_layer_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, precision = self.fitting_precision, trainable = self.trainable[-1])
+            final_layer = one_layer(layer, self.numb_dos, activation_fn = None, name='final_layer_type_'+str(type_i)+suffix, reuse=reuse, seed = self.seed, precision = self.fitting_precision, trainable = self.trainable[-1])
 
-            if type_i < len(self.atom_ener) and self.atom_ener[type_i] is not None:
-                inputs_zero = tf.zeros_like(inputs_i, dtype=GLOBAL_TF_FLOAT_PRECISION)
-                layer = inputs_zero
-                if self.numb_fparam > 0 :
-                    layer = tf.concat([layer, ext_fparam], axis = 1)
-                if self.numb_aparam > 0 :
-                    layer = tf.concat([layer, ext_aparam], axis = 1)
-                for ii in range(0,len(self.n_neuron)) :
-                    if ii >= 1 and self.n_neuron[ii] == self.n_neuron[ii-1] :
-                        layer+= one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=True, seed = self.seed, use_timestep = self.resnet_dt, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, trainable = self.trainable[ii])
-                    else :
-                        layer = one_layer(layer, self.n_neuron[ii], name='layer_'+str(ii)+'_type_'+str(type_i)+suffix, reuse=True, seed = self.seed, activation_fn = self.fitting_activation_fn, precision = self.fitting_precision, trainable = self.trainable[ii])
-                zero_layer = one_layer(layer, 1, activation_fn = None, bavg = type_bias_ae, name='final_layer_type_'+str(type_i)+suffix, reuse=True, seed = self.seed, precision = self.fitting_precision, trainable = self.trainable[-1])
-                final_layer += self.atom_ener[type_i] - zero_layer
-
-            final_layer = tf.reshape(final_layer, [tf.shape(inputs)[0], natoms[2+type_i]])
+            final_layer = tf.reshape(final_layer, [tf.shape(inputs)[0], natoms[2+type_i], self.numb_dos])
 
             # concat the results
             if type_i == 0:
                 outs = final_layer
             else:
                 outs = tf.concat([outs, final_layer], axis = 1)
-
-        if self.tot_ener_zero:
-            force_tot_ener = 0.0
-            outs = tf.reshape(outs, [-1, natoms[0]])
-            outs_mean = tf.reshape(tf.reduce_mean(outs, axis = 1), [-1, 1])
-            outs_mean = outs_mean - tf.ones_like(outs_mean, dtype = GLOBAL_TF_FLOAT_PRECISION) * (force_tot_ener/global_cvt_2_tf_float(natoms[0]))
-            outs = outs - outs_mean
-            outs = tf.reshape(outs, [-1])
 
         tf.summary.histogram('fitting_net_output', outs)
         return tf.cast(tf.reshape(outs, [-1]), GLOBAL_TF_FLOAT_PRECISION)        
